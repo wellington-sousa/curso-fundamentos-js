7e0c0991152a0de611674e0f2d089b10606c826f
Wellington Sousa <wellington.ts@gmail.com>
2022-01-11 16:57:24 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+// boolean
+// true = verdadeiro
+// false = falso
+// Operador "===" comparação tipo e valor
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = false;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2)
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura12345!";
+const stringDeNumeros = "1232345568";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string OU template literal
+
