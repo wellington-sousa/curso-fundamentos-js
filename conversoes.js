4677a79b0cc19b328f0467a1ebeedc86ca85afaf
Wellington Sousa <wellington.ts@gmail.com>
2022-01-12 11:25:56 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// como trabalhar com booleanos
+
+// conversão implícita
+// pode converter um dado com outro
+const numero = 456;
+const numeroString = Number("456");
+
+
+// conversão explicita
+//Number()
+//String()
+
+console.log(numero + numeroString)
